3183c676c0753095ba1aed5419652d5008bb8d1d
thakuranujsingh <thakuranujsingh89@gmail.com>
2021-03-09 14:31:59 +0530

    app.tsx modified

--- ui/src/components/App.tsx
+++ ui/src/components/App.tsx
@@ -1,30 +1,42 @@
 // Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import React from 'react';
-import LoginScreen from './LoginScreen';
-import MainScreen from './MainScreen';
-import DamlLedger from '@daml/react';
-import Credentials from '../Credentials';
-import { httpBaseUrl } from '../config';
+import React from "react";
+import LoginScreen from "./LoginScreen";
+import MainScreen from "./MainScreen";
+import DamlLedger from "@daml/react";
+import Credentials from "../Credentials";
+import { httpBaseUrl } from "../config";
+import { ThemeProviderContainer as Theme } from "../theme";
+import { Snackbar } from "./uiComponents";
 
 /**
  * React component for the entry point into the application.
  */
 // APP_BEGIN
 const App: React.FC = () => {
-  const [credentials, setCredentials] = React.useState<Credentials | undefined>();
+  const [credentials, setCredentials] = React.useState<
+    Credentials | undefined
+  >();
 
-  return credentials
-    ? <DamlLedger
-        token={credentials.token}
-        party={credentials.party}
-        httpBaseUrl={httpBaseUrl}
-      >
-        <MainScreen onLogout={() => setCredentials(undefined)}/>
-      </DamlLedger>
-    : <LoginScreen onLogin={setCredentials} />
-}
+  return (
+    <Theme>
+      <Snackbar maxSnack={6}>
+        {credentials ? (
+          <DamlLedger
+            token={credentials.token}
+            party={credentials.party}
+            httpBaseUrl={httpBaseUrl}
+          >
+            <MainScreen onLogout={() => setCredentials(undefined)} />
+          </DamlLedger>
+        ) : (
+          <LoginScreen onLogin={setCredentials} />
+        )}
+      </Snackbar>
+    </Theme>
+  );
+};
 // APP_END
 
 export default App;

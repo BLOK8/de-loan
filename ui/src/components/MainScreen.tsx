d2eb6d5c9d8e97cad37710aad5323da6f6cde20e
Sahil1109 <sahil.jangra@block8.com>
2021-03-09 15:01:13 +0530

    feat: add google logout button

--- ui/src/components/MainScreen.tsx
+++ ui/src/components/MainScreen.tsx
@@ -1,47 +1,57 @@
 // Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import React from 'react'
-import { Image, Menu } from 'semantic-ui-react'
-import MainView from './MainView';
-import { useParty } from '@daml/react';
-
+import React from "react";
+import { Button, Image, Menu } from "semantic-ui-react";
+import MainView from "./MainView";
+import { useParty } from "@daml/react";
+import { GoogleLogout } from "react-google-login";
 type Props = {
   onLogout: () => void;
-}
+};
 
 /**
  * React component for the main screen of the `App`.
  */
-const MainScreen: React.FC<Props> = ({onLogout}) => {
+const MainScreen: React.FC<Props> = ({ onLogout }) => {
   return (
     <>
       <Menu icon borderless>
         <Menu.Item>
           <Image
-            as='a'
-            href='https://www.daml.com/'
-            target='_blank'
-            src='/daml.svg'
-            alt='DAML Logo'
-            size='mini'
+            as="a"
+            href="https://www.daml.com/"
+            target="_blank"
+            src="/daml.svg"
+            alt="DAML Logo"
+            size="mini"
           />
         </Menu.Item>
-        <Menu.Menu position='right' className='test-select-main-menu'>
-          <Menu.Item position='right'>
+        <Menu.Menu position="right" className="test-select-main-menu">
+          <Menu.Item position="right">
             You are logged in as {useParty()}.
           </Menu.Item>
-          <Menu.Item
+          <GoogleLogout
+            clientId="591508927128-dredtu67nlvlarp64dstg4nbdjq1c146.apps.googleusercontent.com"
+            buttonText="Logout"
+            onLogoutSuccess={onLogout}
+            render={(renderProps) => (
+              <Button basic fluid onClick={renderProps.onClick}>
+                Logout
+              </Button>
+            )}
+          ></GoogleLogout>
+          {/* <Menu.Item
             position='right'
             active={false}
             className='test-select-log-out'
             onClick={onLogout}
             icon='log out'
-          />
+          /> */}
         </Menu.Menu>
       </Menu>
 
-      <MainView/>
+      <MainView />
     </>
   );
 };
